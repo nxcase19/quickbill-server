c6f31bd70cf1bf998332a2d38a8fd9450ec62789
nxcase19 <nxcase19@gmail.com>
2026-04-03 12:15:36 +0700

    fix: watermark grid layout (use CSS grid)

--- src/utils/documentTemplate.js
+++ src/utils/documentTemplate.js
@@ -344,26 +344,26 @@ top: 0;
 left: 0;
 width: 100%;
 height: 100%;
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+grid-template-rows: repeat(4, 1fr);
 z-index: 9999;
 pointer-events: none;
-display: flex;
-flex-wrap: wrap;
-justify-content: center;
-align-items: center;
 ">
 ${Array(12)
   .fill(0)
   .map(
     () => `
   <div style="
-    width: 33%;
-    text-align: center;
-    font-size: 60px;
-    color: rgba(0,0,0,0.05);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 55px;
+    opacity: 0.06;
+    color: #000;
     transform: rotate(-30deg);
     font-weight: 700;
     letter-spacing: 4px;
-    margin: 40px 0;
   ">
     ${watermarkLineEscaped}
   </div>`,
