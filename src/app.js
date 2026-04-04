6768e2af67cf24c1df4cf82301d155c860302dd7
nxcase19 <nxcase19@gmail.com>
2026-04-04 10:38:02 +0700

    fix CORS preflight

--- src/app.js
+++ src/app.js
@@ -29,7 +29,12 @@ const corsOptions = {
   origin: allowedOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Cache-Control',
+    'Pragma',
+  ],
 }
 
 app.use(cors(corsOptions))
